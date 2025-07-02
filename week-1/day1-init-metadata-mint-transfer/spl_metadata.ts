81d69bd10f80b0c55ca11feb3d18cc2918537bfa
7ommyzero <dramaticf@gmail.com>
2025-07-02 09:21:10 -0700

    week 1 day 1 - homework for init + metadata + mint + transfer

---
+++ week-1/day1-init-metadata-mint-transfer/spl_metadata.ts
@@ -0,0 +1,57 @@
+import wallet from "../turbin3-wallet.json"
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
+import { 
+    createMetadataAccountV3, 
+    CreateMetadataAccountV3InstructionAccounts, 
+    CreateMetadataAccountV3InstructionArgs,
+    DataV2Args
+} from "@metaplex-foundation/mpl-token-metadata";
+import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-foundation/umi";
+import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+
+// Define our Mint address
+const mint = publicKey("HKmw98z1e1shtoM6JYnbRzSuD1VdT1c9KrmrcgnfbaJZ")
+
+// Create a UMI connection
+const umi = createUmi('https://api.devnet.solana.com');
+const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+const signer = createSignerFromKeypair(umi, keypair);
+umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
+
+(async () => {
+    try {
+        let accounts: CreateMetadataAccountV3InstructionAccounts = {
+            mint,
+            mintAuthority: signer,
+        }
+
+        let data: DataV2Args = {
+            name: "Gud Token",
+            symbol: "GUD",
+            uri: "https://arweave.net/12345",
+            sellerFeeBasisPoints: 5,
+            creators: null,
+            collection: null,
+            uses: null,
+        }
+
+        let args: CreateMetadataAccountV3InstructionArgs = {
+            data,
+            isMutable: true,
+            collectionDetails: null,
+        }
+
+        let tx = createMetadataAccountV3(
+            umi,
+            {
+                ...accounts,
+                ...args
+            }
+        )
+
+        let result = await tx.sendAndConfirm(umi);
+        console.log(bs58.encode(result.signature));
+    } catch(e) {
+        console.error(`Oops, something went wrong: ${e}`)
+    }
+})();
