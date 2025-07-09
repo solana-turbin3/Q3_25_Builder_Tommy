9d9b70c3418de73a9b49d1d2542fd0f6d3382aba
7ommyzero <dramaticf@gmail.com>
2025-07-09 13:09:04 -0700

    Clean up day2-escrow directory - remove duplicate content

--- week-2/day2-escrow/turbin3-prereqs/TS-prereqs/convert.ts
+++
@@ -1,9 +0,0 @@
-// convert.ts
-import bs58 from 'bs58';
-
-// Replace this with your actual Phantom private key
-const phantomPrivateKey = "";
-
-// Convert to byte array
-const byteArray = bs58.decode(phantomPrivateKey);
-console.log(`[${Array.from(byteArray).join(',')}]`);
